71d6683ed2f7bb0b9400fa97adc2e7485bc001ea
Giovanni Sakti <giosakti@gmail.com>
2018-05-29 11:45:39 +0700

    [gio] setup test

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -4,3 +4,44 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
+
+describe apt('ppa:vbernat/haproxy-1.6') do
+  it { should exist }
+  it { should be_enabled }
+end
+
+describe package('haproxy') do
+  it { should be_installed }
+end
+
+unless os.windows?
+  describe group('haproxy') do
+    it { should exist }
+  end
+
+  describe user('haproxy')  do
+    it { should exist }
+  end
+end
+
+describe file('/etc/haproxy/haproxy.cfg') do
+  its('mode') { should cmp '0644' }
+end
+
+describe file('/etc/rsyslog.d/49-haproxy.conf') do
+  its('mode') { should cmp '0644' }
+end
+
+describe file('/etc/logrotate.d/haproxy') do
+  its('mode') { should cmp '0644' }
+end
+
+describe file('/etc/cron.hourly/logrotate') do
+  its('mode') { should cmp '0755' }
+end
+
+describe systemd_service('haproxy') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
